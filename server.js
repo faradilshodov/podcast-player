593f3c283161396b9abaa549216afd487376ab72
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-03-15 14:35:33 -0400

    Added server.js api call, not finished

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const CryptoJS = require('crypto-js');
+const fetch = require('node-fetch');
+
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello Fara')
+const authKey = process.env.AUTH_KEY;
+const secretKey = process.env.SECRET_KEY;
+const userAgent = process.env.USER_AGENT;
+const apiEndpoint = process.env.API_ENDPOINT;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.listen(3000);
