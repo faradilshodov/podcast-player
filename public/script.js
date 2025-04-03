4356be4d27a2aa6d7b2737df167ef953c0214df9
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-04-03 09:24:57 -0400

    Update scripts and add service worker

--- public/script.js
+++ public/script.js
@@ -615,4 +615,18 @@ document.addEventListener("DOMContentLoaded", () => {
     // On Startup
     loadPlayerState();
     loadQueue();
+
+    // Service Worker ------------------------------------------------------- //
+    if ("serviceWorker" in navigator) {
+        window.addEventListener("load", () => {
+            navigator.serviceWorker
+                .register("service-worker.js")
+                .then((registration) => {
+                    console.log("[Service Worker] registered with scope", registration.scope);
+                })
+                .catch((error) => {
+                    console.error("[Service Worker] registration failed: ", error);
+                });
+        });
+    }
 });
